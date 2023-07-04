648d21683117412d6e3e539537bef9c41be8003c
Shawn Mels <84438879+Shawnmels1998@users.noreply.github.com>
2023-07-04 09:15:54 +0300

    QR code generator

---
+++ QR Code generator/app.py
@@ -0,0 +1,16 @@
+import qrcode
+
+def generate_qrcode(text):
+    qr = qrcode.QRCode(
+        version= 1,
+        error_correction=qrcode.constants.ERROR_CORRECT_L,
+        box_size=10,
+        border=5,
+    )
+    
+    qr.add_data(text)
+    qr.make(fit=True)
+    img = qr.make_image(fill_color="black", back_color="white")
+    img.save("qr1img.png")
+url = input('ENTER YOUR URL: ')   
+generate_qrcode(url)
